886ebcb58254e811291246c07bcbbf1dd95fbc00
Lisa TRAN <ltran@e3r12p22.42.fr>
2017-07-11 20:48:08 +0200

    fdf claquer

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: ltran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/30 15:42:55 by ltran             #+#    #+#             */
-/*   Updated: 2017/07/11 16:57:38 by ltran            ###   ########.fr       */
+/*   Updated: 2017/07/11 18:17:05 by ltran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ void	trace_gril(t_coord *pt, t_tool *t, int zm, char *adr, int *buf)
 		while (++x < pt->y && z < pt->x && (x+1)*zm <= 2560 && y <= 1400)
 		{
 		//	printf("z/(y/zm) %i && pt_x %i pt->y %i  x %i buf|%s|\n", z, pt->x, pt->y, x, buf[x + z*pt->y]);
-			trace(x*zm, (y+(buf[(z*pt->y)+x]*zm)) , (x+1)*zm, (y + (buf[(z*pt->y)+x+1]*zm)) ,adr, t->line);
+			trace(x*zm, (y+(buf[(z*pt->y)+x]*(zm/2)))*(-1) , (x+1)*zm, (y + (buf[(z*pt->y)+x+1])*(zm/2))*(-1) ,adr, t->line);
 		}
 		printf("\n");
 		x = -1;
@@ -56,8 +56,8 @@ void	trace_gril(t_coord *pt, t_tool *t, int zm, char *adr, int *buf)
 	while (++z < pt->y+1 && (y+1)*zm <= 1400 && x <= 2560)
 	{
 		x = z*zm;
-		while (++y < pt->x && z < pt->y && (y+1)*zm <= 1400 && x <= 2560)
-			trace(x, (y)*zm, x,( y+1)*zm ,adr, t->line);
+		while (++y < pt->x &&  z < pt->y && (y+1)*zm <= 1400 && x <= 2560)
+			trace(x, ((y+buf[y*z+z])*((zm/2)*(-1))), x, (y+1+buf[(y+1)*z+z])*((zm/2)*(-1)) ,adr, t->line);
 		y = -1;
 	}
 }
@@ -68,7 +68,7 @@ void	start_window(char **map, t_coord *pt, t_tool *t, int *buf)
 	int		z = -1;
 	int		x = -1;
 	char	*adr;
-	int		zm = 50;
+	int		zm = 100;
 	int i = -1;
 
 	t->mlx = mlx_init ();
